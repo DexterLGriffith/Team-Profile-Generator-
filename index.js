d06db520e71b2bfce5c9153ff7e3f86dd677fd77
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-10-09 15:23:13 -0500

    making general shells for tests and files.

---
+++ index.js
@@ -0,0 +1,45 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+//prompt questions for TEAM BUILDING 
+inquirer, prompt([
+
+
+
+
+
+
+
+
+
+
+
+
+
+])
+//get it to write the index.html and CSS file.  
+.then((data) => {
+    fs.writeFile(
+        if () {
+
+        }
+        else {
+
+        }
+    )};
+
+});
+
+//how i did readme generator. 
+// .then((data) => {
+//     // get it to write onto a readme file. 
+//         fs.writeFile('generatedREADME.md', generateReadMe(data),(err) => {
+//             if (err) {
+//                 console.log('ERROR',err);
+//             }
+//             else {
+//                 console.log('SUCCESS you created a README.md file!');
+//             }
+//         });
+//     });
