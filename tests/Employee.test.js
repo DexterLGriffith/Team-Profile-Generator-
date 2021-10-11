ba25935ca7a2697d86831b6ef2221e747126c238
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-10-11 00:32:27 -0500

    mainly trying to get employee tests to run on employee.test.js

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,10 +1,23 @@
 const Employee = require('../Lib/Employee')
 
-describe("EmployeeTest", () => {
+describe("employeeTest", () => {
     it("can create employee object", () => {
         const employee = new Employee()
 
-        expect(typeof(employee)).toEqual(object)
+        expect(typeof(employee)).toEqual("object")
     });
 })
-    
+describe("employeeTest", () => {
+    it("can create name within employee object", () => {
+        const employee = new Employee("name");
+
+        expect(employee.name).toEqual("name");
+    });
+})
+describe("employeeTest", () => {
+    it("can create ID within employee object", () => {
+        const id = new Employee("id")
+
+        expect(employee.id).toEqual("id")
+    });
+})
