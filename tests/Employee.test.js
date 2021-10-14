a809c18998083e555f39977484660febf53cf049
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-10-13 22:57:00 -0500

    trying to add email test for employee.

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -25,6 +25,24 @@ describe("employeeTest", () => {
         expect(e.id).toEqual(idTest)
     });
 })
+describe("employeeTest", () => {
+    it("can create email within employee object", () => {
+        // get email 
+        const email = tester@testy.com;
+        const e = new Employee("Test", email)
+       
+
+        expect(e.email).toEqual(email)
+    });
+// describe("employeeTest", () => {
+//     it("can create role within employee object", () => {
+//        //get role
+//         const idTest = 50;
+//         const e = new Employee("Test", idTest)
+       
+
+//         expect(e.id).toEqual(idTest)
+//     });
 
 // from notes   describe("isPalindrome", () => {
     // it("should return true if a string is a palindrome", () => {
