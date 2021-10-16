69e033d7bc468727340854b48a861ef81991fc27
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-10-16 18:28:47 -0500

    working on the generatedHTML page. almost done

--- generateHTML.js
+++ generateHTML.js
@@ -4,8 +4,12 @@ const renderedTeam = team => {
     }
 
     // repeat for intern 
-    const renderedIntern = intern
+    const renderedIntern = intern => {
+        return `${Intern.getName()},${Intern.getId()},${Intern.getEmail()},${Intern.getSchool()},${Intern.getRole()}`
+    }
 
     // repeat for engineer
-    const renderedEngineer = engineer
+    const renderedEngineer = engineer => {
+        return `${Intern.getName()},${Intern.getId()},${Intern.getEmail()},${Intern.getGithub()},${Intern.getRole()}`
+    }
 }
